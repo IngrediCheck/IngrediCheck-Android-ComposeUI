80cf924e447461d21d35a8e1d2cac36b69b4967a
Gaurav-eightinity01 <gaurav.gawali@eightinity.in>
2025-09-09 14:23:49 +0530

    one more commit and API Level change to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "lc.fungee.IngrediCheck"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "llc.fungee.IngrediCheck"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
